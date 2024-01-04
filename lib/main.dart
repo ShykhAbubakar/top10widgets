51d997e28662ac1cbec40a57bf096fb26066b2d5
Abubakar Javed <shykhabubakar99@gmail.com>
2024-01-04 17:25:32 +0500

    stack widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,13 @@ class MyApp extends StatelessWidget {
                   Text('top10widgets', style: TextStyle(color: Colors.white))),
           backgroundColor: Colors.blue,
         ),
-        body: Center(
+        body:  const Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
 
-              // Widget Number # 01: Container Properties
+              // Widget Number # 01: Container Properties source code
 
               // Container(
               //   transform: Matrix4.rotationZ(0.2),
@@ -55,7 +55,7 @@ class MyApp extends StatelessWidget {
               //       )),
               // ),
 
-              // Widget Number 2 : Expanded Widget
+              // Widget Number 2 : Expanded Widget source code
 
 
               // Column(
@@ -115,35 +115,33 @@ class MyApp extends StatelessWidget {
               //         )),
               //   ),
               // ),
-              Container(
-                width: 200,
-                height: 200,
-                transform: Matrix4.rotationZ(0.2),
-                child: const Center(child: Text('Container')),
-                decoration: BoxDecoration(
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black,
-                        blurRadius: 50,
-                      )
-                    ],
-                    color: Colors.greenAccent,
-                    border: Border.all(
-                      width: 10,
-                      color: Colors.black87,
-                    ),
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(50),
-                      bottomRight: Radius.circular(50),
-                      topRight: Radius.circular(50),
-                      bottomLeft: Radius.circular(50),
-                    ),
-                    image: const DecorationImage(
-                      image: NetworkImage(
-                          'https://www.shutterstock.com/image-photo/just-beautiful-cute-smiling-baby-600nw-2144454063.jpg'),
-                      fit: BoxFit.fitHeight,
-                    )),
-              ),
+
+              // Widget Number 3 : Stack  Widget source code
+
+              // Stack(
+              //   alignment: Alignment.centerRight,
+              //   children: [
+              //     Container(
+              //       width: 140,
+              //       height: 100,
+              //       color: Colors.deepOrangeAccent,
+              //     ),
+              //     Container(
+              //       width: 120,
+              //       height: 100,
+              //       color: Colors.greenAccent,
+              //     ),
+              //     Container(
+              //       width: 80,
+              //       height: 100,
+              //       color: Colors.blue,
+              //     ),
+              //     const Positioned(child: const Text('Abubakar '),
+              //     bottom: 30,
+              //     )
+              //
+              //   ],
+              // ),
             ],
           ),
         ),
