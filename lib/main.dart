81cd9587f3ac04876c573f5ae3c022a1556cf4d6
Abubakar Javed <shykhabubakar99@gmail.com>
2024-01-04 15:27:03 +0500

    image uploaded

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,56 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        
+        appBar: AppBar(
+          title: const Center(
+              child:
+                  Text('top10widgets', style: TextStyle(color: Colors.white))),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                child: const Center(child: Text('Container')),
+                decoration: BoxDecoration(
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 50,
+                      )
+                    ],
+                    color: Colors.greenAccent,
+                    border: Border.all(
+                      width: 10,
+                      color: Colors.black87,
+                    ),
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      bottomRight: Radius.circular(50),
+                      topRight: Radius.circular(50),
+                      bottomLeft: Radius.circular(50),
+                    ),
+                    image: const DecorationImage(
+                      image: NetworkImage(
+                          'https://www.shutterstock.com/image-photo/just-beautiful-cute-smiling-baby-600nw-2144454063.jpg'),
+                    )),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
